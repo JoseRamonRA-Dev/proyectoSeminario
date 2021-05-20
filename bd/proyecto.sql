26da0a1f34803731ec1ca7e9fe1b2d24f14b786c
AdriDeLe238 <deleonadriana681@gmail.com>
2021-05-20 12:58:49 -0500

    Login, y base de datos

---
+++ bd/proyecto.sql
@@ -0,0 +1,65 @@
+use empatines;
+create database empatines; 
+
+create table Responsable(
+	ID smallint not null auto_increment,
+	Nombre char(30) not null,
+	Apellido_P char(20) not null,
+	Apellido_M char(20),
+    correo char(60),
+	Telefono  numeric(10),
+    ID_Org smallint not null,
+	foreign key (ID_Org)
+	references Organizacion(ID),
+    primary key (ID)
+);
+
+create table Organizacion(
+ID smallint not null auto_increment,
+Nombre char(30) not null,
+Calle char(35),
+Colonia char(20),
+CP numeric(5),
+Ciudad char(20),
+Estado char(20) not null,
+Telefono  numeric(10),
+primary key (ID)
+);
+
+create table Persona(
+ID smallint not null auto_increment,
+Nombre char(30) not null,
+Apellido_P char(20) not null,
+Apellido_M char(20),
+Fecha_Nac date not null,
+correo char(60),
+contraseña char(15),
+Telefono  numeric(10),
+ID_Org smallint not null,
+foreign key (ID_Org)
+	references Organizacion(ID),
+ID_Resp smallint not null,
+foreign key (ID_Resp)
+	references Responsable(ID),
+ID_Perfil smallint not null,
+foreign key (ID_Perfil)
+	references Perfil(ID),
+primary key (ID)
+);
+
+create table perfil(
+	ID smallint not null auto_increment,
+	Resultado char(40) not null,
+    Fecha_Nac date not null,
+primary key(ID)
+);
+
+select * from perfil;
+
+
+
+
+
+
+
+
